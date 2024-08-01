daf4e70417c3f41faca6e799e7b7340ed31b1b32
Markfla <147580286+Markfla@users.noreply.github.com>
2024-08-01 19:51:55 +0100

    added board class logic

--- Tic-Tac-Toe/board.rb
+++ Tic-Tac-Toe/board.rb
@@ -15,4 +15,38 @@ class Board
     puts "|  #{@positions[6]}  |  #{@positions[7]}  |  #{@positions[8]}  |"
     puts "|_____|_____|_____|"
   end
+
+  def valid_move?(position)
+    position.between?(0, 8) && @positions[position] == '-'
+  end
+
+  def update_position(position, marker)
+    @positions[position] = marker if valid_move?(position)
+  end
+
+  def win?
+    winning_combinations = [
+      [0, 1, 2],  # Top row
+      [3, 4, 5],  # Middle row
+      [6, 7, 8],  # Bottom row
+      [0, 3, 6],  # Left column
+      [1, 4, 7],  # Middle column
+      [2, 5, 8],  # Right column
+      [0, 4, 8],  # Diagonal from top-left to bottom-right
+      [2, 4, 6]   # Diagonal from top-right to bottom-left
+    ]
+
+    winning_combinations.each do |combo|
+      if @positions[combo[0]] != '-' &&
+         @positions[combo[0]] == @positions[combo[1]] &&
+         @positions[combo[1]] == @positions[combo[2]]
+        return true
+      end
+    end
+    false
+  end
+
+  def full?
+    !@positions.include?('-')
+  end
 end
