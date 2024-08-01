f9995b1f8d888d8d0f92374efa441257e1b44953
Markfla <147580286+Markfla@users.noreply.github.com>
2024-08-01 17:38:04 +0100

    used rubocop-performance on main

--- Caeser Cipher/main.rb
+++ Caeser Cipher/main.rb
@@ -1,23 +1,24 @@
-def charArrayToInt(unEncryptedArray, rightShift)
-  unEncryptedArray.map do |char|
+# frozen_string_literal: true
+
+def char_array_to_int(unencrypted_array, right_shift)
+  unencrypted_array.map do |char|
     # Convert character to ASCII, shift it, and wrap around if necessary
     if char.match?(/[A-Za-z]/)
       base = char.ord < 91 ? 65 : 97
-      ((char.ord - base + rightShift) % 26 + base).chr
+      (((char.ord - base + right_shift) % 26) + base).chr
     else
       char
     end
   end
 end
 
-def caesar_cipher(unEncrypted, rightShift)
-  unEncryptedArray = unEncrypted.split('')
-  intArray = charArrayToInt(unEncryptedArray, rightShift)
-  encryptedString = intArray.join('')
-  return encryptedString
+def caesar_cipher(unencrypted, right_shift)
+  unencrypted_array = unencrypted.chars
+  int_array = char_array_to_int(unencrypted_array, right_shift)
+  int_array.join
 end
 
 # Example usage:
-puts caesar_cipher("Hello, World!", 3)  # Khoor, Zruog!
-puts caesar_cipher("abcXYZ", 2)         # cdeZAB
-puts caesar_cipher("xyz", -3)           # uvw
+puts caesar_cipher('Hello, World!', 3)  # Khoor, Zruog!
+puts caesar_cipher('abcXYZ', 2)         # cdeZAB
+puts caesar_cipher('xyz', -3)           # uvw
