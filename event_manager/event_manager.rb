5087b9806bce362cac3cd105d66d91dff34885e4
Markfla <147580286+Markfla@users.noreply.github.com>
2024-08-13 14:48:29 +0100

    added time targetting and returns most common hours for registration

--- event_manager/event_manager.rb
+++ event_manager/event_manager.rb
@@ -3,20 +3,20 @@ require 'google/apis/civicinfo_v2'
 require 'erb'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 def clean_number(number)
   number_str = number.to_s.gsub(/\D/, '')
-  puts "number string : " + number_str
 
   if number_str.length == 10
-    return number
+    number_str
   elsif number_str.length == 11 && number_str[0] == '1'
-    return number[1..10]
+    number_str[1..10]
   else
-    return '0000000000'
+    '0000000000'
   end
+end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -28,12 +28,16 @@ def legislators_by_zipcode(zip)
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     ).officials
-  rescue
+  rescue Google::Apis::ClientError => e
+    puts "Error: #{e.message}"
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  rescue StandardError => e
+    puts "An unexpected error occurred: #{e.message}"
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -54,15 +58,29 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+hourly_counts = Hash.new(0)
+peak_hours = 0
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   number = clean_number(row[:homephone])
-  puts (name +" " +number + "\n")
+  puts "#{name} - Cleaned Number: #{number}"
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
+
+  registration_time = DateTime.strptime(row[:regdate], '%m/%d/%Y %H:%M')
+  hour = registration_time.hour
+
+  hourly_counts[hour] += 1
+  peak_hours = hourly_counts.select { |hour, count| count == hourly_counts.values.max }
 end
+
+puts "peak hours => " + peak_hours
+
+
